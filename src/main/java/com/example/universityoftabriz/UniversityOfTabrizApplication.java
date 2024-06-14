46acfd989dd8b0c4e9a877bf74d6bb52ff4b8c71
hamed005 <hamednobakht949@gmail.com>
2024-06-14 18:27:25 +0330

    first commit

---
+++ src/main/java/com/example/universityoftabriz/UniversityOfTabrizApplication.java
@@ -0,0 +1,13 @@
+package com.example.universityoftabriz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityOfTabrizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityOfTabrizApplication.class, args);
+    }
+
+}
