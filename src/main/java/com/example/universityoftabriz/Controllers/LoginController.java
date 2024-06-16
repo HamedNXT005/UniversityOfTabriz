0974a80f722bc11fca0684f82391b4d41e7d65c6
Elyar KordKatool <elyar.k@yahoo.com>
2024-06-16 13:49:54 +0330

    First Push

--- src/main/java/com/example/universityoftabriz/Controllers/LoginController.java
+++ src/main/java/com/example/universityoftabriz/Controllers/LoginController.java
@@ -1,4 +1,28 @@
 package com.example.universityoftabriz.Controllers;
 
+import com.example.universityoftabriz.Objects.Student;
+import com.example.universityoftabriz.Services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
 public class LoginController {
+
+    @Autowired
+    private StudentService studentService;
+    @RequestMapping("/")
+    public String Login(){
+        return "Login";
+    }
+
+    @GetMapping("/api/getStudents")
+    @ResponseBody
+    public List<Student> getAllStudents(){
+        return studentService.getStudent();
+    }
 }
---
+++ src/main/java/com/example/universityoftabriz/Services/StudentService.java
@@ -0,0 +1,19 @@
+package com.example.universityoftabriz.Services;
+
+import com.example.universityoftabriz.Objects.Student;
+import com.example.universityoftabriz.Repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public List<Student> getStudent(){
+        return studentRepository.findAll();
+    }
+}
