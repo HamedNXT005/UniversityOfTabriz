d9c66b5585b173ed091c53fcf5b187c3aab1c02c
Elyar KordKatool <elyar.k@yahoo.com>
2024-07-01 13:22:18 +0330

    initial service commit for review.

--- src/main/java/com/example/universityoftabriz/Services/ResourcesService.java
+++ src/main/java/com/example/universityoftabriz/Services/ResourcesService.java
@@ -1,12 +1,34 @@
 package com.example.universityoftabriz.Services;
 
+import com.example.universityoftabriz.Objects.Resources;
 import com.example.universityoftabriz.Repository.ResourcesRepository;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ResourcesService {
     @Autowired
     private ResourcesRepository resourcesRepository;
 
+    public void updateBudget(Long id, long budget, Logger logger) {
+        Optional<Resources> resourceOptional = resourcesRepository.findById(id);
+        if (resourceOptional.isPresent()) {
+            Resources resource = resourceOptional.get();
+            resource.setTotal_budget(budget);
+            resourcesRepository.save(resource);
+            logger.info("New budget set successfully.");
+        } else {
+            logger.error("Resource with id: {} not found.",id);
+            throw new RuntimeException("Resource with id: " + id + " not found.");
+        }
+    }
+
+    public void updateBudget(Resources resource, long budget, Logger logger) {
+            resource.setTotal_budget(budget);
+            resourcesRepository.save(resource);
+            logger.info("New budget set successfully.");
+    }
 }
